b11dd691c3992e3e9f08453f351b349063da31bc
showyou <showyou41@gmail.com>
2019-12-20 00:44:27 +0900

    add reply rule

--- src/crawler.py
+++ src/crawler.py
@@ -3,6 +3,10 @@
 
 import sys
 import os
+import json
+import datetime
+from   sqlalchemy import and_
+import random
 
 # /home/*/hama_dbとかが返ってくる
 #exec_path = os.path.abspath(os.path.dirname(__file__)).rsplit("/",1)[0]
@@ -10,11 +14,8 @@ exec_path = "."
 conf_path = exec_path+"/common/config.json"
 sys.path.insert(0,exec_path)
 from common import auth_api, model
+import tweepy
 
-import json
-import datetime
-from   sqlalchemy import and_
-import random
 
 #ログに入れない人のリスト
 g_ngUser = [ 
@@ -34,9 +35,49 @@ def get_auth_data(fileName):
 
 # NGUserならTrue そうでないならFalse 
 def is_ng_user(user):
-	for u in g_ngUser:
-		if u == user: return True
-	return False
+    for u in g_ngUser:
+        if u == user: return True
+    return False
+
+
+def check_reply(reply_id):
+    if(reply_id is not None and\
+        int(reply_id) != -1):
+        return False
+    return True
+
+
+def check_none(reply_id):
+    return True
+
+
+"""
+    テキストが適合している = True
+    重複してたり、RTだったり = False
+"""
+def check_text(text, reply_id, name, created_at, id, dbSession, check_func):
+    if( text.startswith("RT ")): return False
+    print("rep: ", reply_id)
+    if(not(check_func(reply_id))): return False
+    #if s.author.screen_name == userdata["user"]:continue
+            
+    jTime = created_at + datetime.timedelta(hours = 9)
+    if( is_ng_user(name) ): return False
+    query = dbSession.query(model.Tweet).filter(
+        and_(
+            model.Tweet.user==name,
+            model.Tweet.tweetID == id
+        )
+    )
+    if( query.count() > 0 ): return False
+    t = model.Tweet()
+    t.user = name
+    t.text = text
+    t.datetime = jTime
+    t.replyID = reply_id
+    t.tweetID = id
+    dbSession.add(t)
+    return True
 
 
 def main():
@@ -52,52 +93,48 @@ def main():
     update_flag = True
     while update_flag:
         update_flag = False
-        #l = tw.home_timeline(page = page_number, count=100)
-        l = tw.search("かすかす", count=10)
         page_number += 1
         if page_number > 2: break
+
+        l = tw.search("かすかす", count=10)
         for s in l:
-            if( s.text.startswith("RT ")): continue
-            print("rep: ", s.in_reply_to_status_id)
-            if( s.in_reply_to_status_id is not None and\
-                int(s.in_reply_to_status_id) != -1 and\
-                not(s.text.startswith("@NakasukasumiB")))): continue
-            #if s.author.screen_name == userdata["user"]:continue
-            
-            jTime = s.created_at + datetime.timedelta(hours = 9)
-            name = s.user.screen_name
-            if( is_ng_user(name) ): continue
-            query = dbSession.query(model.Tweet).filter(
-                and_(
-                    model.Tweet.user==name,
-                    model.Tweet.tweetID == s.id
-                )
-            )
-            if( query.count() > 0 ): continue
-            update_flag = True
+            update_flag = check_text(s.text, s.in_reply_to_status_id,
+                    s.user.screen_name, s.created_at,
+                    s.id, dbSession, check_reply)
+            if(not(update_flag)): continue
+            message = random.choice([
+                "かすかすじゃなくってかすみんです！",
+                "かすみん！",
+                "だから、かすかすじゃなくってかすみんですってば！"
+            ])
+            try:
+                tw.update_status("@"+s.user.screen_name+" "+message, s.in_reply_to_status_id)
+            except tweepy.TweepError:
+                pass
+        dbSession.commit()
+        
+    page_number = 0
+    update_flag = True
+    while update_flag:
+        update_flag = False
+        page_number += 1
+        if page_number > 2: break
 
-            t = model.Tweet()
-            t.user = name
-            t.text = s.text
-            t.datetime = jTime
-            t.replyID = s.in_reply_to_status_id
-            t.tweetID = s.id
-            #print "id:",s.id, 
-            dbSession.add(t)
-
-            message =""
-            if(s.text.startswith("@NakasukasumiB")):
-                message = random.choice([
-                    "せんぱい、コッペパン焼いてきましたよ！", 
-                    "えへへっ！"
-                ])
-            else:
-                message = random.choice([
-                    "かすかすじゃなくってかすみんです！",
-                    "かすみん！",
-                    "だから、かすかすじゃなくってかすみんですってば！"
-                ])
-            tw.update_status("@"+name+" "+message, t.replyID)
+        l = tw.search("@NakasukasumiB", count=10)
+        for s in l:
+            update_flag = check_text(s.text, s.in_reply_to_status_id,
+                    s.user.screen_name, s.created_at,
+                    s.id, dbSession, check_none)
+            if(not(update_flag)): continue
+            print("user: "+s.user.screen_name)
+            message = random.choice([
+                "せんぱい、コッペパン焼いてきましたよ！", 
+                "えへへっ！"
+            ])
+            try:
+                tw.update_status("@"+s.user.screen_name+" "+message, s.in_reply_to_status_id)
+            except tweepy.TweepError:
+                pass
         dbSession.commit()
 
 
